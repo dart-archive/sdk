19f54d3a601093420dd5b1d0def5a22ab0612ff4
Peter von der Ahé <ahe@google.com>
2015-02-09 15:05:12 +0100

    Add disassembled bytecodes as comments.
    R=kasperl@google.com
    
    Review URL: https://codereview.chromium.org/881073005

--- pkg/fletchc/lib/hello_world_commands.dart
+++ pkg/fletchc/lib/hello_world_commands.dart
@@ -15,12 +15,30 @@ const List<Command> commands = const <Command>[
 
     const PushNewString("Hej Verden!"),
 
+    // method @1: _printString
+    //    0: invoke native 1 0
+    //    3: throw
+    //    4: method end 4
     const PushNewFunction(
         1, 0, const [26, 1, 0, 61, 71, 4, 0, 0, 0, 0, 0, 0, 0]),
 
+    // method @2: _halt
+    //    0: invoke native 1 1
+    //    3: throw
+    //    4: method end 4
     const PushNewFunction(
         1, 0, const [26, 1, 1, 61, 71, 4, 0, 0, 0, 0, 0, 0, 0]),
 
+    // method @0: _entry
+    //    0: load const @0
+    //    5: invoke static @1
+    //   10: pop
+    //   11: load literal 1
+    //   12: invoke static @2
+    //   17: pop
+    //   18: load literal null
+    //   19: return 1 1
+    //   22: method end 22
     const PushNewFunction(
         1, 3,
         const [
