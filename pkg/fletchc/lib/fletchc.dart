54215905b82ea3c06869aad24ea60977268498ca
Peter von der Ahé <ahe@google.com>
2015-02-12 11:03:18 +0100

    Rename dart2js_bridge to fletchc.
    R=kasperl@google.com
    
    Review URL: https://codereview.chromium.org/916813003

--- pkg/fletchc/lib/fletchc.dart
+++ pkg/fletchc/lib/fletchc.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE.md file.
 
-library fletchc.dart2js_bridge;
+library fletchc;
 
 import 'compiler.dart' show
     FletchCompiler;
