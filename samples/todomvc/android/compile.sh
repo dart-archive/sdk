c81cecd5f6ff20158b016ee57541d92335f97f63
Mads Ager <ager@google.com>
2015-03-09 11:58:30 +0100

    Add all files to the Android studio project to get a working sample.
    As far as I can tell it is not possible to point to other
    directories, so it seems that we have to copy everything
    in here. Fortunately, the steps are simple:
    
    1. Generate the java and jni code and copy the java code.
    
     [tools/servicec]$ dart ./bin/servicec.dart --out=../../samples/todomvc/ ../../samples/todomvc/todomvc_service.idl
     [samples/todomvc/java]$ cp -R fletch/* ../android/TodoMVC/app/src/main/java/fletch/
    
    2. Build libfletch.so which contains all of the fletch runtime
       and the jni code and copy it.
    
     [samples/todomvc/java]$ NDK_MODULE_PATH=. ndk-build
     [samples/todomvc/java]$ libs/* ../android/TodoMVC/app/src/main/jniLibs/
    
    3. Build the snapshot and copy it.
    
     [samples/todomvc]$ ../../out/ReleaseIA32/fletch todomvc.dart --out=todomvc_snapshot
     [samples/todomvc]$ cp todomvc_snapshot android/TodoMVC/app/src/main/res/raw/
    
    R=zerny@google.com
    
    Review URL: https://codereview.chromium.org/984913003

---
+++ samples/todomvc/android/compile.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright (c) 2015, the Fletch project authors. Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE.md file.
+
+set -ue
+
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+SERVICEC_DIR=$DIR/../../../tools/servicec
+JAVA_DIR=$DIR/../java
+FLETCH_DIR=$DIR/../../..
+
+# Regenerate java and jni sources.
+cd $SERVICEC_DIR
+dart bin/servicec.dart --out=../../samples/todomvc/ ../../samples/todomvc/todomvc_service.idl
+
+# Compile Fletch runtime and jni code into libfletch.so.
+cd $JAVA_DIR
+NDK_MODULE_PATH=. ndk-build
+
+# Copy Java source and fletch library to the right places.
+mkdir $DIR/TodoMVC/app/src/main/java/fletch
+cp -R fletch/*.java $DIR/TodoMVC/app/src/main/java/fletch/
+mkdir $DIR/TodoMVC/app/src/main/jniLibs/
+cp -R libs/* $DIR/TodoMVC/app/src/main/jniLibs/
+
+# Build snapshot.
+cd $FLETCH_DIR
+mkdir $DIR/TodoMVC/app/src/main/res/raw
+./out/ReleaseIA32/fletch $DIR/../todomvc.dart --out=$DIR/TodoMVC/app/src/main/res/raw/todomvc_snapshot
+
