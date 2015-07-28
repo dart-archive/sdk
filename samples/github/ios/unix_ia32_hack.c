b03efc00bd1921738fa75c706181f88ea1a9c927
Sarah Winther Zakarias <zarah@google.com>
2015-07-28 14:42:17 +0200

    Update unix_ia32_hack.c
    R=zerny@google.com
    
    Review URL: https://codereview.chromium.org/1260943002 .

--- samples/github/ios/unix_ia32_hack.c
+++ samples/github/ios/unix_ia32_hack.c
@@ -14,3 +14,7 @@ FILE* fopen$UNIX2003(const char* filename, const char* mode) {
 size_t fwrite$UNIX2003(const void* a, size_t b, size_t c, FILE* d) {
   return fwrite(a, b, c, d);
 }
+
+int fputs$UNIX2003(const char *restrict s, FILE *restrict stream) {
+  return fputs(s, stream);
+}
