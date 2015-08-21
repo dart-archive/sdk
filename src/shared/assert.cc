9d0488fd41f921db7c50e907c04be4c4af15d41e
Mads Ager <ager@google.com>
2015-08-21 07:39:41 +0200

    Remove dynamic stack trace generation on assertion failures.
    When most symbols are hidden it leads to stacktraces with hex numbers
    instead of symbols which is not very useful. We'll use gdb and core
    files to analyze crashes from the bots.
    
    R=kustermann@google.com
    
    Review URL: https://codereview.chromium.org/1298183004 .

--- src/shared/assert.cc
+++ src/shared/assert.cc
@@ -27,8 +27,6 @@ void DynamicAssertionHelper::Fail(const char* format, ...) {
   va_end(arguments);
   Print::Error("%s\n", buffer);
 
-  DumpStacktrace();
-
   // In case of failed assertions, abort right away. Otherwise, wait
   // until the program is exiting before producing a non-zero exit
   // code through abort.
@@ -37,72 +35,5 @@ void DynamicAssertionHelper::Fail(const char* format, ...) {
   if (!failed) std::atexit(std::abort);
   failed = true;
 }
-
-// NOTE: This function uses compiler-specific features to retrieve
-// the current stacktrace.
-void DynamicAssertionHelper::DumpStacktrace() {
-#ifndef __ANDROID__
-  const int kMaxFrames = 200;
-  void* buffer[kMaxFrames];
-
-  int number_of_frames = backtrace(buffer, kMaxFrames);
-  char** strings = backtrace_symbols(buffer, number_of_frames);
-  if (strings != NULL) {
-    Print::Error("\nCurrent stacktrace:\n");
-    for (int i = 0; i < number_of_frames; i++) {
-      char* string = strings[i];
-
-      // Normally a frame string looks like:
-      //    "<binary-name>(<mangled_name>+<hex-offset>)"
-      // We try to extract <mangled_name> and demangle it.
-      char* open_paren = NULL;
-      char* plus = NULL;
-      char* close_paren = NULL;
-      int pos = 0;
-      while (string[pos] != '\0') {
-        if (string[pos] == '(') {
-          open_paren = &string[pos];
-        } else if (string[pos] == '+') {
-          plus = &string[pos];
-        } else if (string[pos] == ')') {
-          close_paren = &string[pos];
-        }
-        pos++;
-      }
-
-      // If we found the markers, we'll try to demangle the name.
-      if (open_paren != NULL && plus != NULL && close_paren != NULL) {
-        int mangled_name_length = plus - open_paren - 1;
-        char* mangled_name =
-            static_cast<char*>(malloc(mangled_name_length + 1));
-        memcpy(mangled_name, open_paren + 1, mangled_name_length);
-        mangled_name[mangled_name_length] = '\0';
-
-        int status;
-        char* demangled_string = abi::__cxa_demangle(
-            mangled_name, NULL, NULL, &status);
-        if (status == 0) {
-          *open_paren = '\0';
-          Print::Error("  %s(%s%s\n", string, demangled_string, plus);
-        } else {
-          Print::Error("  %s\n", string);
-        }
-        free(demangled_string);
-
-        free(mangled_name);
-      } else {
-        Print::Error("  %s\n", string);
-      }
-    }
-    Print::Error("\n");
-    free(strings);
-  } else {
-    Print::Error("\nCould not get a stacktrace.\n");
-  }
-#else
-  Print::Error("\nCannot get a stacktrace on android.\n");
-#endif
-}
-
 
 }  // namespace fletch
--- src/shared/assert.h
+++ src/shared/assert.h
@@ -47,8 +47,6 @@ class DynamicAssertionHelper {
 #endif
 
  private:
-  void DumpStacktrace();
-
   const char* const file_;
   const int line_;
   const Kind kind_;
