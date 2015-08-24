96ae3a6a61dde510c4c988f457baa1fa32c7faff
Sarah Winther Zakarias <zarah@google.com>
2015-08-24 09:34:35 +0200

    Github sample: remove top action bar on expanded cards.
    R=zerny@google.com
    
    Review URL: https://codereview.chromium.org/1302383002 .

--- samples/github/android/GithubSample/app/src/main/java/com/google/fletch/githubsample/DetailsViewActivity.java
+++ samples/github/android/GithubSample/app/src/main/java/com/google/fletch/githubsample/DetailsViewActivity.java
@@ -17,6 +17,7 @@ public class DetailsViewActivity extends Activity{
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_details);
+    getActionBar().hide();
     configureCard();
   }
 
