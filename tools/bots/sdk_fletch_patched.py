05a19b608e83912423d88a2fb114ca01235ea852
Rico Wind <ricow@google.com>
2015-09-25 11:27:18 +0200

    Put patched dart sdk builds into a "subfolder" of the fletch-archive bucket
    R=kasperl@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1373463002 .

--- tools/bots/sdk_fletch_patched.py
+++ tools/bots/sdk_fletch_patched.py
@@ -37,7 +37,7 @@ def BuildSteps(build_info):
                                         build_info.mode,
                                         build_info.arch)
     revision = utils.GetGitRevision()
-    name = 'fletch-archive/%s/dart-vm-%s-%s' % (
+    name = 'fletch-archive/patched_dart_sdks/%s/dart-vm-%s-%s' % (
         revision, build_info.arch, build_info.system)
     download_link = 'https://storage.googleapis.com/%s' % name
     gcs_path = 'gs://%s' % name
