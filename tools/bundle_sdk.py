5c7e801bde790a4aff5695b137472c2dae087f9f
Rico Wind <ricow@google.com>
2015-09-29 14:29:01 +0200

    Add script for creating an sdk bundle This is dependent on: https://codereview.chromium.org/1374163003/
    I originally a large set of the lists of things to move/copy at the top of the file, but with some of the special casing it actually made it more readable having it in place in the functions. I am happy to move more stuff out of the functions if requested.
    
    I will upload testing support in a seperate cl to not have to big a change.
    
    R=sgjesse@google.com, wibling@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1372083004 .

--- tools/bundle_sdk.py
+++ tools/bundle_sdk.py
@@ -1,7 +1,141 @@
+#!/usr/bin/env python
 # Copyright (c) 2015, the Fletch project authors.  Please see the AUTHORS file
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
 # This script is creating a self contained directory with all the tools,
-# libraries, packages and samples needed for running fletch. Please try to
-# keep this simple (hacks in here makes failures hard to debug).
+# libraries, packages and samples needed for running fletch.
+
+# This script assumes that the target arg has been build in the passed in
+# --build_dir. It also assumes that out/ReleaseXARM/fletch-vm has been build.
+
+import optparse
+import subprocess
+import sys
+import utils
+
+from os import makedirs
+from os.path import join, exists, basename
+from shutil import copyfile, copymode, copytree, rmtree
+
+SDK_PACKAGES = ['file', 'fletch_agent', 'fletchc', 'gpio',
+                'http', 'i2c', 'immutable', 'os', 'raspberry_pi', 'socket']
+
+def ParseOptions():
+  parser = optparse.OptionParser()
+  parser.add_option("--build_dir")
+  (options, args) = parser.parse_args()
+  return options
+
+def CopyFile(src, dst):
+  copyfile(src, dst)
+  copymode(src, dst)
+
+def EnsureDeleted(directory):
+  if exists(directory):
+    rmtree(directory)
+  if exists(directory):
+    raise Exception("Could not delete %s" % directory)
+
+def CopyBinaries(bundle_dir, build_dir):
+  bin_dir = join(bundle_dir, 'bin')
+  internal = join(bundle_dir, 'internal')
+  makedirs(bin_dir)
+  makedirs(internal)
+  for v in ['fletch-vm', 'natives.json']:
+    CopyFile(join(build_dir, v), join(bin_dir, v))
+  # The driver for the sdk is specially named fletch_for_sdk.
+  CopyFile(join(build_dir, 'fletch_for_sdk'), join(bin_dir, 'fletch'))
+  # We move the dart vm to internal to not put it on the path of users
+  CopyFile(join(build_dir, 'dart'), join(internal, 'dart'))
+
+# We have two lib dependencies: the libs from the sdk and the libs dir with
+# patch files from the fletch repo.
+def CopyLibs(bundle_dir, build_dir):
+  internal = join(bundle_dir, 'internal')
+  fletch_lib = join(internal, 'fletch_lib')
+  dart_lib = join(internal, 'dart_lib')
+  copytree('lib', join(fletch_lib, 'lib'))
+  copytree('../dart/sdk/lib', join(dart_lib, 'lib'))
+
+def CopyInternalPackages(bundle_dir, build_dir):
+  internal_pkg = join(join(bundle_dir, 'internal'), 'pkg')
+  makedirs(internal_pkg)
+  for v in ['fletchc', 'fletch_agent']:
+    copytree(join('pkg', v), join(internal_pkg, v))
+  fletchc = 'pkg/fletchc'
+  fixed_packages_file = join(join(internal_pkg, 'fletchc'), '.sdk_packages')
+  with open(join(fletchc, '.packages')) as f:
+    with open(fixed_packages_file, 'w') as generated:
+      for l in f.read().splitlines():
+        if l.startswith('#') or l.startswith('fletchc:lib'):
+          generated.write('%s\n' % l)
+        else:
+          components = l.split(':')
+          name = components[0]
+          relative_path = components[1]
+          source = join(fletchc, relative_path)
+          target = join(internal_pkg, name)
+          print 'copying %s to %s' % (source, target)
+          makedirs(target)
+          assert(source.endswith('lib'))
+          copytree(source, join(target, 'lib'))
+          generated.write('%s:../%s/lib\n' % (name, name))
+
+def CopyPackages(bundle_dir):
+  target_dir = join(bundle_dir, 'pkg')
+  makedirs(target_dir)
+  with open(join(join(bundle_dir, 'bin'), 'fletch-sdk.packages'), 'w') as p:
+    for package in SDK_PACKAGES:
+      copytree(join('pkg', package), join(target_dir, package))
+      p.write('%s:../pkg/%s\n' % (package, package))
+
+def CopyPlatforms(bundle_dir):
+  target_dir = join(bundle_dir, 'platforms')
+  copytree('platforms', target_dir)
+
+def CreateAgentSnapshot(bundle_dir):
+  data_dir = join(join(bundle_dir, 'raspberry-pi2'), 'data')
+  makedirs(data_dir)
+  snapshot = join(data_dir, 'fletch-agent.snapshot')
+  bin_snapshot = join(join(bundle_dir, 'bin'), 'fletch-agent.snapshot')
+  cmd = ['out/ReleaseIA32/fletch', 'export', 'pkg/fletch_agent/bin/agent.dart',
+         'to', snapshot]
+  print 'Running %s' % ' '.join(cmd)
+  subprocess.check_call(cmd)
+  CopyFile(snapshot, bin_snapshot)
+
+def CopyAdditionalFiles(bundle_dir):
+  #TODO(mit): fix README and LICENSE
+  for extra in ['README.md', 'LICENSE.md']:
+    CopyFile(extra, join(bundle_dir, extra))
+
+def CopyArm(bundle_dir):
+  binaries = ['fletch-vm', 'natives.json']
+  raspberry = join(join(bundle_dir, 'platforms'), 'raspberry-pi2')
+  bin_dir = join(raspberry, 'bin')
+  makedirs(bin_dir)
+  build_dir = 'out/ReleaseXARM'
+  for v in binaries:
+    CopyFile(join(build_dir, v), join(bin_dir, v))
+  # TODO(ricow): add deb package
+
+def Main():
+  options = ParseOptions();
+  print 'Creating sdk bundle for %s' % options.build_dir
+  build_dir = options.build_dir
+  with utils.TempDir() as sdk_temp:
+    CopyBinaries(sdk_temp, build_dir)
+    CopyInternalPackages(sdk_temp, build_dir)
+    CopyLibs(sdk_temp, build_dir)
+    CopyPackages(sdk_temp)
+    CopyPlatforms(sdk_temp)
+    CopyArm(sdk_temp)
+    CreateAgentSnapshot(sdk_temp)
+    CopyAdditionalFiles(sdk_temp)
+    sdk_dir = join(build_dir, 'fletch-sdk')
+    EnsureDeleted(sdk_dir)
+    copytree(sdk_temp, sdk_dir)
+
+if __name__ == '__main__':
+  sys.exit(Main())
