f0270e9f5d8ca6b0813dce63d04330d95e52958f
Michael Thomsen <mit@google.com>
2015-09-30 11:19:07 +0200

    Basic Raspberry Pi 2 samples
    BUG=
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org/1366403004 .

---
+++ samples/raspberry_pi/basic/blinky.dart
@@ -0,0 +1,26 @@
+// Copyright (c) 2015, the Fletch project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE.md file.
+//
+// The 'hello world' of embedded: Blinking an LED. Uses the on-board
+// Raspberry Pi 2 activity LED.
+
+import 'package:gpio/gpio.dart';
+import 'package:os/os.dart';
+
+main(List<String> args) {
+  // Constant for the Raspberry Pi 2 onboard green LED.
+  const int led = 47;
+
+  // Initialize gpio and configure the pin.
+  PiMemoryMappedGPIO gpio = new PiMemoryMappedGPIO();
+  gpio.setMode(led, Mode.output);
+
+  // Turn LED on and off in a continuous loop.
+  while (true) {
+    gpio.setPin(led, true);
+    sleep(500);
+    gpio.setPin(led, false);
+    sleep(500);
+  }
+}
