b08a600cfe24a4b115fe957baa7d91de4b32edb4
Kasper Lund <kasperl@google.com>
2015-10-02 13:44:27 +0200

    Make 'fletch-vm' friendlier to CLI users.
    See issue https://github.com/dart-lang/fletch/issues/181.
    
    R=ahe@google.com, lukechurch@google.com, ricow@google.com, sgjesse@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1377463004 .

--- src/vm/main.cc
+++ src/vm/main.cc
@@ -29,23 +29,23 @@ static int RunSession(Connection* connection) {
   return result;
 }
 
-static void WriteIntToFile(const char* dirPath, const char* ext, int value) {
-  char filePath[MAXPATHLEN + 1];
-  snprintf(filePath, sizeof(filePath), "%s/vm-%d.%s",
-      dirPath, Platform::GetPid(), ext);
-  char valueString[20];
-  snprintf(valueString, sizeof(valueString), "%d", value);
-  Platform::WriteText(filePath, valueString, false);
+static void WriteIntToFile(const char* dir_path, const char* ext, int value) {
+  char file_path[MAXPATHLEN + 1];
+  snprintf(file_path, sizeof(file_path), "%s/vm-%d.%s",
+      dir_path, Platform::GetPid(), ext);
+  char value_string[20];
+  snprintf(value_string, sizeof(value_string), "%d", value);
+  Platform::WriteText(file_path, value_string, false);
 }
 
 static Connection* WaitForCompilerConnection(
-    const char* host, int port, const char* runDir) {
+    const char* host, int port, const char* run_dir) {
   // Listen for new connections.
   ConnectionListener listener(host, port);
 
   Print::Out("Waiting for compiler on %s:%i\n", host, listener.Port());
-  if (runDir != NULL) {
-    WriteIntToFile(runDir, "port", listener.Port());
+  if (run_dir != NULL) {
+    WriteIntToFile(run_dir, "port", listener.Port());
   }
   return listener.Accept();
 }
@@ -54,10 +54,22 @@ static bool IsSnapshot(List<uint8> snapshot) {
   return snapshot.length() > 2 && snapshot[0] == 0xbe && snapshot[1] == 0xef;
 }
 
-static void printUsage() {
-  Print::Out("fletch-vm - The Dart virtual machine for IOT.\n\n");
+static bool StartsWith(const char* s, const char* prefix) {
+  return strncmp(s, prefix, strlen(prefix)) == 0;
+}
+
+static bool EndsWith(const char* s, const char* suffix) {
+  int s_length = strlen(s);
+  int suffix_length = strlen(suffix);
+  return (suffix_length <= s_length)
+      ? strncmp(s + s_length - suffix_length, suffix, suffix_length) == 0
+      : false;
+}
+
+static void PrintUsage() {
+  Print::Out("fletch-vm - The embedded Dart virtual machine.\n\n");
   Print::Out("  fletch-vm [--port=<port>] [--host=<address>] "
-             "[snaphost file]\n\n");
+             "[snapshot file]\n\n");
   Print::Out("When specifying a snapshot other options are ignored.\n\n");
   Print::Out("Options:\n");
   Print::Out("  --port: specifies which port to listen on. Defaults "
@@ -73,48 +85,52 @@ static int Main(int argc, char** argv) {
   FletchSetup();
 
   if (argc > 5) {
-    FATAL("Too many arguments.");
+    Print::Out("Too many arguments.\n\n");
+    PrintUsage();
+    exit(1);
   } else if (argc < 1) {
-    FATAL("Not enough arguments.");
+    Print::Out("Not enough arguments.\n\n");
+    PrintUsage();
+    exit(1);
   }
 
   // Handle the arguments.
   const char* host = "127.0.0.1";
-  const char* runDir = NULL;
-  const char* logDir = NULL;
+  const char* run_dir = NULL;
+  const char* log_dir = NULL;
   int port = 0;
   const char* input = NULL;
 
   // We run a snapshot only if the arguments contain a file name.
-  bool runSnapshot = false;
-  bool invalidOption = false;
+  bool run_snapshot = false;
+  bool invalid_option = false;
 
   for (int i = 1; i < argc; ++i) {
     const char* argument = argv[i];
-    if (strncmp(argument, "--port=", 7) == 0) {
+    if (StartsWith(argument, "--port=")) {
       port = atoi(argument + 7);
-    } else if (strncmp(argument, "--host=", 7) == 0) {
+    } else if (StartsWith(argument, "--host=")) {
       host = argument + 7;
-    } else if (strncmp(argument, "--help", 6) == 0) {
-      printUsage();
+    } else if (strcmp(argument, "--help") == 0) {
+      PrintUsage();
       exit(0);
-    } else if (strncmp(argument, "--log-dir=", 10) == 0) {
-      logDir = argument + 10;
-    } else if (strncmp(argument, "--run-dir=", 10) == 0) {
-      runDir = argument + 10;
-    } else if (strncmp(argument, "-", 1) == 0) {
+    } else if (StartsWith(argument, "--log-dir=")) {
+      log_dir = argument + 10;
+    } else if (StartsWith(argument, "--run-dir=")) {
+      run_dir = argument + 10;
+    } else if (StartsWith(argument, "-")) {
       Print::Out("Invalid option: %s.\n", argument);
-      invalidOption = true;
+      invalid_option = true;
     } else {
       // No matching option given, assume it is a snapshot.
       input = argument;
-      runSnapshot = true;
+      run_snapshot = true;
     }
   }
-  if (invalidOption) {
+  if (invalid_option) {
     // Don't continue if one or more invalid/unknown options were passed.
     Print::Out("\n");
-    printUsage();
+    PrintUsage();
     exit(1);
   }
   bool interactive = true;
@@ -123,28 +139,39 @@ static int Main(int argc, char** argv) {
 
   // Check if we should add a log print interceptor.
   int pid = Platform::GetPid();
-  if (logDir != NULL) {
+  if (log_dir != NULL) {
     // Generate a vm specific log name with the given path.
-    char logPath[MAXPATHLEN + 1];
-    snprintf(logPath, sizeof(logPath), "%s/vm-%d.log", logDir, pid);
-    Print::RegisterPrintInterceptor(new LogPrintInterceptor(logPath));
+    char log_path[MAXPATHLEN + 1];
+    snprintf(log_path, sizeof(log_path), "%s/vm-%d.log", log_dir, pid);
+    Print::RegisterPrintInterceptor(new LogPrintInterceptor(log_path));
   }
 
   // Write the pid to a file if a run directory (e.g. /var/run/fletch) is set.
-  if (runDir != NULL) {
-    WriteIntToFile(runDir, "pid", pid);
+  if (run_dir != NULL) {
+    WriteIntToFile(run_dir, "pid", pid);
   }
 
   // Check if we're passed an snapshot file directly.
-  if (runSnapshot) {
+  if (run_snapshot) {
     List<uint8> bytes = Platform::LoadFile(input);
+    if (bytes.is_empty()) {
+      Print::Out("\n");  // Separate error from Platform::LoadFile from usage.
+      PrintUsage();
+      exit(1);
+    }
     if (IsSnapshot(bytes)) {
       FletchProgram program = FletchLoadSnapshot(bytes.data(), bytes.length());
       result = FletchRunMain(program);
       FletchDeleteProgram(program);
       interactive = false;
     } else {
-      FATAL("File is not a snapshot.");
+      Print::Out("The file '%s' is not a snapshot.\n\n");
+      if (EndsWith(input, ".dart")) {
+        Print::Out("Try: 'fletch run %s'\n\n", input);
+      } else {
+        PrintUsage();
+      }
+      exit(1);
     }
     bytes.Delete();
   }
@@ -155,7 +182,7 @@ static int Main(int argc, char** argv) {
   if (interactive) {
     // When interactive and a pid directory is specified write the port we are
     // listening on to the file vm-<pid>.port in the pid directory.
-    Connection* connection = WaitForCompilerConnection(host, port, runDir);
+    Connection* connection = WaitForCompilerConnection(host, port, run_dir);
     result = RunSession(connection);
   }
 
