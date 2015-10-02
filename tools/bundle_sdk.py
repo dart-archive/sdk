6f30880121e03e6ca499da08ff433e0812c0a48e
Rico Wind <ricow@google.com>
2015-10-02 20:26:43 +0200

    Remove immutable package from sdk, add general samples to sdk
    Closes #184
    
    R=kasperl@google.com, kustermann@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1380263003 .

--- tools/bundle_sdk.py
+++ tools/bundle_sdk.py
@@ -18,10 +18,9 @@ from os import makedirs
 from os.path import join, exists, basename
 from shutil import copyfile, copymode, copytree, rmtree
 
-SDK_PACKAGES = ['file', 'gpio', 'http', 'i2c', 'immutable', 'os',
-                'raspberry_pi', 'socket']
+SDK_PACKAGES = ['file', 'gpio', 'http', 'i2c', 'os', 'raspberry_pi', 'socket']
 
-SAMPLES = ['raspberry_pi']
+SAMPLES = ['raspberry_pi', 'general']
 
 def ParseOptions():
   parser = optparse.OptionParser()
