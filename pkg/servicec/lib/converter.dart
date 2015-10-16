6cc8efae9676913affe5ccc11a133897d55464ca
Stan Manilov <stanm@google.com>
2015-10-16 13:53:04 +0200

    Fix ordering and keep multiple fields per struct
    The compiler used to output just a single field per struct and the order
    of everything was reversed (services, structs, arguments, etc.). This CL
    fixes the problem.
    
    BUG=
    R=zerny@google.com
    
    Review URL: https://codereview.chromium.org/1407233002 .

--- pkg/servicec/lib/converter.dart
+++ pkg/servicec/lib/converter.dart
@@ -10,109 +10,82 @@ import 'node.dart' show
     FormalNode,
     FunctionNode,
     ListType,
+    MemberNode,
     PointerType,
     RecursiveVisitor,
     ServiceNode,
     SimpleType,
     StructNode,
+    TopLevelNode,
     TypeNode,
     UnionNode;
 
 import 'package:old_servicec/src/parser.dart' as old;
 
+import 'dart:collection' show
+    Queue;
+
 // Validation functions.
 old.Unit convert(CompilationUnitNode compilationUnit) {
-  Converter converter = new Converter();
-  converter.visitCompilationUnit(compilationUnit);
-  return converter.nodeStack.removeLast();
+  Iterable<TopLevelNode> services =
+    compilationUnit.topLevels.where((topLevel) => topLevel is ServiceNode);
+  Iterable<TopLevelNode> structs =
+    compilationUnit.topLevels.where((topLevel) => topLevel is StructNode);
+  return new old.Unit(
+      services.map(convertService).toList(),
+      structs.map(convertStruct).toList());
 }
 
-class Converter extends RecursiveVisitor {
-  List<old.Node> nodeStack = <old.Node>[];
+old.Service convertService(ServiceNode service) {
+  return new old.Service(
+      service.identifier.value,
+      service.functions.map(convertFunction).toList());
+}
 
-  void visitCompilationUnit(CompilationUnitNode compilationUnit) {
-    super.visitCompilationUnit(compilationUnit);
-    List<old.Service> services = <old.Service>[];
-    List<old.Struct> structs = <old.Struct>[];
-    while (nodeStack.isNotEmpty) {
-      old.Node top = nodeStack.removeLast();
-      if (top is old.Service) {
-        services.add(top);
-      }
-      if (top is old.Struct) {
-        structs.add(top);
-      }
-    }
-    nodeStack.add(new old.Unit(services, structs));
-  }
+old.Struct convertStruct(StructNode struct) {
+  Iterable<MemberNode> fields =
+    struct.members.where((member) => member is FieldNode);
+  Iterable<MemberNode> unions =
+    struct.members.where((member) => member is UnionNode);
+  return new old.Struct(
+      struct.identifier.value,
+      fields.map(convertField).toList(),
+      unions.map(convertUnion).toList());
+}
 
-  void visitService(ServiceNode service) {
-    super.visitService(service);
-    List<old.Method> methods = <old.Method>[];
-    while (nodeStack.isNotEmpty && nodeStack.last is old.Method) {
-      methods.add(nodeStack.removeLast());
-    }
-    nodeStack.add(new old.Service(service.identifier.value, methods));
-  }
+old.Method convertFunction(FunctionNode function) {
+  return new old.Method(
+      function.identifier.value,
+      function.formals.map(convertFormal).toList(),
+      convertType(function.returnType));
+}
 
-  void visitStruct(StructNode struct) {
-    super.visitStruct(struct);
-    List<old.Formal> slots = <old.Formal>[];
-    List<old.Union> unions = <old.Union>[];
-    while (nodeStack.isNotEmpty) {
-      old.Node top = nodeStack.last;
-      if (top is old.Formal) {
-        slots.add(top);
-      }
-      if (top is old.Union) {
-        unions.add(top);
-      } else {
-        break;
-      }
-      nodeStack.removeLast();
-    }
-    nodeStack.add(new old.Struct(struct.identifier.value, slots, unions));
-  }
+old.Formal convertFormal(FormalNode formal) {
+  return new old.Formal(
+      convertType(formal.type),
+      formal.identifier.value);
+}
 
-  void visitFunction(FunctionNode function) {
-    super.visitFunction(function);
-    List<old.Formal> arguments = <old.Formal>[];
-    while (nodeStack.isNotEmpty && nodeStack.last is old.Formal) {
-      arguments.add(nodeStack.removeLast());
-    }
-    old.Type type = convertType(function.returnType);
-    nodeStack.add(new old.Method(function.identifier.value, arguments, type));
-  }
+old.Formal convertField(FieldNode field) {
+  return new old.Formal(
+      convertType(field.type),
+      field.identifier.value);
+}
 
-  void visitFormal(FormalNode formal) {
-    old.Type type = convertType(formal.type);
-    nodeStack.add(new old.Formal(type, formal.identifier.value));
-  }
+old.Union convertUnion(UnionNode union) {
+  return new old.Union(
+      union.fields.map(convertField).toList());
+}
 
-  void visitUnion(UnionNode union) {
-    super.visitUnion(union);
-    List<old.Formal> slots = <old.Formal>[];
-    while (nodeStack.isNotEmpty && nodeStack.last is old.Formal) {
-      slots.add(nodeStack.removeLast());
-    }
-    nodeStack.add(new old.Union(slots));
-  }
-
-  void visitField(FieldNode field) {
-    old.Type type = convertType(field.type);
-    nodeStack.add(new old.Formal(type, field.identifier.value));
-  }
-
-  old.Type convertType(TypeNode type) {
-    if (type.isString()) {
-      return new old.StringType();
-    } else if (type.isPointer()) {
-      return new old.SimpleType(type.identifier.value, true);
-    } else if (type.isList()) {
-      ListType listType = type;
-      return new old.ListType(convertType(listType.typeParameter));
-    } else {
-      return new old.SimpleType(type.identifier.value, false);
-    }
+old.Type convertType(TypeNode type) {
+  if (type.isString()) {
+    return new old.StringType();
+  } else if (type.isPointer()) {
+    return new old.SimpleType(type.identifier.value, true);
+  } else if (type.isList()) {
+    ListType listType = type;
+    return new old.ListType(convertType(listType.typeParameter));
+  } else {
+    return new old.SimpleType(type.identifier.value, false);
   }
 }
