a9a88864cbf035c7484da5702a6ababf52089e57
Stan Manilov <stanm@google.com>
2015-10-20 11:17:28 +0200

    Remove files generated for service tests
    These files are generated by the tests
     * fletch_tests/service_tests/conformance and
     * fletch_tests/service_tests/performance
    as of the last commit (12be86bd0f6a2bcbf9feae99014548f1344fd707).
    
    They should no longer be in the repository.
    
    Update .gitignore files to ignore generated folders.
    
    BUG=
    R=kustermann@google.com
    
    Review URL: https://codereview.chromium.org/1414893002 .

--- tests/service_tests/conformance/java/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2015, the Fletch project authors. Please see the AUTHORS file
-# for details. All rights reserved. Use of this source code is governed by a
-# BSD-style license that can be found in the LICENSE.md file.
-
-# Generated file. Do not edit.
-APP_STL := gnustl_static
-APP_ABI := x86 armeabi-v7a
-APP_PLATFORM := android-8
