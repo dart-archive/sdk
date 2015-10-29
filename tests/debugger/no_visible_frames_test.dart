fa81e731e788e840a3b042d4567688d3dac496bb
Mads Ager <ager@google.com>
2015-10-29 17:03:56 +0100

    Add debugger tests of stack-trace printing when there are only internal frames.
    Remove status file that is no longer used. The status file for debugger
    tests is now fletch_tests/fletch_tests.status
    
    R=kustermann@google.com, ahe@google.com, wibling@google.com
    
    Review URL: https://codereview.chromium.org/1406933004 .

---
+++ tests/debugger/no_visible_frames_test.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, the Fletch project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE.md file.
+
+// FletchDebuggerCommands=r,bt,t internal,q
+
+// Tests that stack traces work in the debugger when there are
+// no visible frames (in that case 'bt' doesn't print anything).
+// This program will hit a compile-time error when calling
+// main in internal non-user code.
+void main(a, b, c) {}
