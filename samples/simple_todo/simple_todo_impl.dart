f91577801f4a8a9088a523217ade39270f40d471
Stan Manilov <stanm@google.com>
2015-11-02 15:38:25 +0100

    Fix library name and import
    Fix the broken build as of a few commits ago.
    
    BUG=
    TO BE REVIEWED
    
    Review URL: https://codereview.chromium.org/1416633009 .

--- samples/simple_todo/simple_todo_impl.dart
+++ samples/simple_todo/simple_todo_impl.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE.md file.
 
-import 'model.dart';
+import 'todo_model.dart';
 import 'dart/simple_todo.dart';
 
 class TodoImpl extends TodoService {
--- samples/simple_todo/todo_model.dart
+++ samples/simple_todo/todo_model.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE.md file.
 
-library todomvc.model;
+library todomvc.todo_model;
 
 // Very simple model for a collection of TODO items.
 
