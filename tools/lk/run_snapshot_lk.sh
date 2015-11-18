b93221b8086d9e696df4982bcb95353a13afa609
Stephan Herhut <herhut@google.com>
2015-11-18 12:38:47 +0100

    Automatically find serial port for ST-Link device.
    BUG=
    R=bak@google.com
    
    Review URL: https://codereview.chromium.org/1461583002 .

--- tools/lk/run_snapshot_lk.sh
+++ tools/lk/run_snapshot_lk.sh
@@ -3,16 +3,25 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE.md file.
 
+source $(dirname $(readlink -f $0))/devdiscovery.shlib
+
 if [ -z "$1" -o ! -s "$1" ]; then
 echo "$0: Expecting a snapshot file as fist argument."
 exit 1
 fi
-PORT=${2-/dev/ttyUSB0}
+
+if [ -z "$PORT" ]; then
+  discover_devices
+  PORT=${STLINKPORT}
+fi
 
 if [ ! -c "$PORT" ]; then
 echo "$0: $PORT is not a valid character device."
 exit 2
 fi
+
+# Configure the port to LK's default tty speed.
+stty -F $PORT 115200
 
 SIZE=`cat $1 | wc -c`
 
