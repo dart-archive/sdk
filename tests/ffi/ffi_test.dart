941f1c26e08a6809ea98b39f440190d66faca62c
Rico Wind <ricow@google.com>
2015-11-30 09:59:53 +0100

    Remove left over test function for ForeignCString
    This got left out of 07d60695cde002d4cb046218a0c672a2f8593f0b
    
    R=wibling@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1480253002 .

--- tests/ffi/ffi_test.dart
+++ tests/ffi/ffi_test.dart
@@ -617,33 +617,3 @@ testStruct() {
   struct32.free();
   fl.close();
 }
-
-testForeignCString() {
-  var memory = new ForeignMemory.allocated(100);
-  memory.setUint8(0, 65);
-  memory.setUint8(1, 0);
-  Expect.equals(
-      'A', new ForeignCString.fromNullTerminated(memory).toString());
-  Expect.equals(
-      '', new ForeignCString(memory, 0).toString());
-  Expect.equals(
-      'A', new ForeignCString(memory, 1).toString());
-  memory.setUint8(1, 42);
-  Expect.equals(
-      'A', new ForeignCString(memory, 1).toString());
-  memory.setUint8(0, 0xc3);
-  memory.setUint8(1, 0x98);
-  memory.setUint8(2, 0);
-  Expect.equals(
-      'Ø', new ForeignCString.fromNullTerminated(memory).toString());
-  memory.free();
-
-  var libPath = ForeignLibrary.bundleLibraryName('ffi_test_library');
-  ForeignLibrary fl = new ForeignLibrary.fromName(libPath);
-  var memstring = fl.lookup('memstring');
-  var foreignPointer = memstring.pcall$0();
-  Expect.equals(
-      'dart', new ForeignCString.fromNullTerminated(foreignPointer).toString());
-  memory = new ForeignMemory.fromAddress(foreignPointer.address, 5);
-  memory.free();
-}
