77e0498599cb2b23f603705da8ea30ab1ffcba9c
Mads Ager <ager@google.com>
2015-12-02 15:30:03 +0100

    Create baseline-disco built configuration.
    Switch to LKFull for the stm-disco-fletch target.
    
    This allows us to very easily measure the binary size requirements
    for fletch over time.
    
    Build baseline-disco in release mode, build stm-disco-fletch in release
    mode, look at the difference in the reported sizes at the end of the
    build.
    
    R=herhut@google.com, ajohnsen@google.com
    
    Review URL: https://codereview.chromium.org/1493733003 .

--- third_party/lk/fletch/project/stm32f746g-disco-baseline.mk
+++ third_party/lk/fletch/project/stm32f746g-disco-baseline.mk
@@ -4,12 +4,7 @@
 
 include project/target/stm32f746g-disco.mk
 
-MODULES += app/fletch-fixed lib/gfx
-
-EXTRA_LINKER_SCRIPTS += fletch/project/add-fletch-snapshot-section.ld
-
-FLETCH_CONFIGURATION = LK
-FLETCH_GYP_DEFINES = "LK_PROJECT=stm32f746g-disco-fletch LK_CPU=cortex-m4"
+MODULES += app/shell lib/gfx
 
 WITH_CPP_SUPPORT=true
 
--- third_party/lk/fletch/project/stm32f746g-disco-fletch.mk
+++ third_party/lk/fletch/project/stm32f746g-disco-fletch.mk
@@ -1,8 +1,12 @@
+# Copyright (c) 2015, the Fletch project authors. Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE.md file.
+
 include project/target/stm32f746g-disco.mk
 
 MODULES += app/fletch app/shell lib/gfx
 
-FLETCH_CONFIGURATION = LK
+FLETCH_CONFIGURATION = LKFull
 FLETCH_GYP_DEFINES = "LK_PROJECT=stm32f746g-disco-fletch LK_CPU=cortex-m4"
 
 WITH_CPP_SUPPORT=true
