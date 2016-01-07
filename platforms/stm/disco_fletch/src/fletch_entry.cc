354a543abb5591be3266cd32c6ac361f7c577990
Søren Gjesse <sgjesse@google.com>
2016-01-07 10:29:59 +0100

    Update the stack size for the main thread for FreeRTOS
    This makes the FreeRTOS disco_fletch port run again.
    
    This matches the LK change in 532f3e0b3700b9189e66b6a55d4643322c4ecef2.
    
    R=ricow@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1570463002 .

--- platforms/stm/disco_fletch/src/fletch_entry.cc
+++ platforms/stm/disco_fletch/src/fletch_entry.cc
@@ -56,7 +56,8 @@ extern "C" void FletchEntry(void const * argument) {
   uart = new Uart();
   uart->Start();
 
-  osThreadDef(START_FLETCH, StartFletch, osPriorityNormal, 0, 1024);
+  osThreadDef(START_FLETCH, StartFletch, osPriorityNormal, 0,
+              8 * 1024 /* stack size */);
   osThreadCreate(osThread(START_FLETCH), NULL);
 
   // No more to do right now.
