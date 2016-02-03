df18b78653833081490bd365b9fc356f97312c5a
Ian Zerny <zerny@google.com>
2016-02-03 11:30:29 +0100

    Fix flaky list-processes test, take 2.
    This inserts another timer to give the child process time to terminate
    before hitting the break point.
    
    R=kustermann@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1658863003 .

--- tests/debugger/list_processes_test.dart
+++ tests/debugger/list_processes_test.dart
@@ -8,31 +8,35 @@
 
 import 'dart:async';
 import 'dart:fletch';
+import 'package:expect/expect.dart';
 
-Port spawnPaused(Channel channel) {
+Port spawnPaused(Channel channel, Channel monitor) {
   var port = new Port(channel);
   Process.spawnDetached(() {
-    var c = new Channel();
-    port.send(new Port(c));
-    // TODO(zerny): Make this a CLI test since without the timer, the child
-    // process might not make it to the following receive.
-    var echo = c.receive();
-    port.send(echo);
-  });
+      var c = new Channel();
+      port.send(new Port(c));
+      var echo = c.receive();  // B1
+      port.send(echo);
+    },
+    monitor: new Port(monitor));
   return channel.receive();
 }
 
 void resume(Channel channel, Port port) {
   port.send(42);
-  var result = channel.receive();
+  Expect.equals(42, channel.receive());
 }
 
 main() {
   Channel channel = new Channel();
-  Port p1 = spawnPaused(channel);
-  Port p2 = spawnPaused(channel);
+  Channel monitor = new Channel();
+  Port p1 = spawnPaused(channel, monitor);
+  Port p2 = spawnPaused(channel, monitor);
+  // Give the children time to block at B1.
   Timer timer = new Timer(const Duration(seconds: 2), () {
     resume(channel, p2);
+    Expect.equals(DeathReason.Terminated, monitor.receive().reason);
     resume(channel, p1);
+    Expect.equals(DeathReason.Terminated, monitor.receive().reason);
   });
 }
