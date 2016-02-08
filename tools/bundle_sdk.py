dc8c9e6d2f7d3ef9639626966f507e705795dda0
Rico Wind <ricow@google.com>
2016-02-08 08:41:27 +0100

    Ship the mbedtls shared libary as part of the sdk
    For now this is only raspberry pi and linux/mac, I will update with a library file that we can link against for stm when I get it up and running there
    
    R=sgjesse@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1673143002 .

--- tools/bundle_sdk.py
+++ tools/bundle_sdk.py
@@ -24,7 +24,8 @@ from shutil import copyfile, copymode, copytree, rmtree, ignore_patterns
 TOOLS_DIR = abspath(dirname(__file__))
 
 SDK_PACKAGES = ['ffi', 'file', 'dartino', 'gpio', 'http', 'i2c', 'os',
-                'raspberry_pi', 'stm32f746g_disco', 'socket', 'mqtt']
+                'raspberry_pi', 'stm32f746g_disco', 'socket', 'mqtt',
+                'mbedtls']
 THIRD_PARTY_PACKAGES = ['charcode']
 
 SAMPLES = ['general', 'raspberry-pi2', 'stm32f746g-discovery']
@@ -61,6 +62,22 @@ def EnsureDeleted(directory):
   if exists(directory):
     raise Exception("Could not delete %s" % directory)
 
+def CopySharedLibraries(bin_dir, build_dir):
+  shared_libraries = ['mbedtls']
+  # Libraries are placed differently on mac and linux:
+  # Linux has lib/libNAME.so
+  # Mac has libNAME.dylib
+  os_name = utils.GuessOS()
+  lib_dst = join(bin_dir, 'lib') if os_name == 'linux' else bin_dir
+  lib_src = join(build_dir, 'lib') if os_name == 'linux' else build_dir
+  suffix = 'so' if os_name == 'linux' else 'dylib'
+  makedirs(lib_dst)
+  for lib in shared_libraries:
+    lib_name = 'lib%s.%s' % (lib, suffix)
+    src = join(lib_src, lib_name)
+    dst = join(lib_dst, lib_name)
+    CopyFile(src, dst)
+
 def CopyBinaries(bundle_dir, build_dir):
   bin_dir = join(bundle_dir, 'bin')
   internal = join(bundle_dir, 'internal')
@@ -73,6 +90,7 @@ def CopyBinaries(bundle_dir, build_dir):
   CopyFile(join(build_dir, 'dart'), join(internal, 'dart'))
   # natives.json is read relative to the dart binary
   CopyFile(join(build_dir, 'natives.json'), join(internal, 'natives.json'))
+  CopySharedLibraries(bin_dir, build_dir)
 
 # Copy the platform decriptor, rewriting paths to point to the
 # sdk location at `sdk_dir` instead of `repo_dir`.
