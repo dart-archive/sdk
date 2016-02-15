dart-archive/sdk

commit 37e79c13b069717dba6bfbce5bb0615fc0975fe2

Pass arguments to the main, when starting a new process at main entry point.
BUG=https://github.com/dartino/sdk/issues/18
R=sgjesse@google.com

Review URL: https://codereview.chromium.org/1684903004 .

src/vm/scheduler.cc
@@ -697,6 +697,7 @@ SimpleProgramRunner::~SimpleProgramRunner() {
 void SimpleProgramRunner::Run(int count,
                               int* exitcodes,
                               Program** programs,
+                              int argc, char** argv,
                               Process** processes) {
   programs_ = programs;
   exitcodes_ = exitcodes;
@@ -710,7 +711,12 @@ void SimpleProgramRunner::Run(int count,
 
     program->SetProgramExitListener(
         &SimpleProgramRunner::CaptureExitCode, this);
-    if (process == NULL) process = program->ProcessSpawnForMain();
+    List<List<uint8>> arguments = List<List<uint8>>::New(argc);
+    for (int i = 0; i < argc; i++) {
+      uint8* utf8 = reinterpret_cast<uint8*>(strdup(argv[i]));
+      arguments[i] = List<uint8>(utf8, strlen(argv[i]));
+    }
+    if (process == NULL) process = program->ProcessSpawnForMain(arguments);
     scheduler->ScheduleProgram(program, process);
   }
 

include/dartino_api.h
@@ -63,22 +63,30 @@ DARTINO_EXPORT DartinoProgram DartinoLoadProgramFromFlash(void* location,
 //     DartinoDeleteProgram)
 //   * use thread-local storage - at least in debug mode - which ensures this.
 DARTINO_EXPORT void DartinoStartMain(DartinoProgram program,
-                                   ProgramExitCallback callback,
-                                   void* callback_data);
+                                     ProgramExitCallback callback,
+                                     void* callback_data,
+                                     int argc,
+                                     char** argv);
 
 // Run the main method of the program and wait until it is done executing.
-DARTINO_EXPORT int DartinoRunMain(DartinoProgram program);
+DARTINO_EXPORT int DartinoRunMain(DartinoProgram program,
+                                  int argc,
+                                  char** argv);
 
 // Run the main method of multiple programs and wait until all of them are done
 // executing.
 DARTINO_EXPORT void DartinoRunMultipleMain(int count,
-                                         DartinoProgram* programs,
-                                         int* exitcodes);
+                                           DartinoProgram* programs,
+                                           int* exitcodes,
+                                           int argc,
+                                           char** argv);
 
 // Load the snapshot from the file, load the program from the
 // snapshot, run the main process of that program and wait until it is done
 // executing.
-DARTINO_EXPORT void DartinoRunSnapshotFromFile(const char* path);
+DARTINO_EXPORT void DartinoRunSnapshotFromFile(const char* path,
+                                               int argc,
+                                               char** argv);
 
 // Add a default shared library for the dart:ffi foreign lookups.
 // More than one default shared library can be added. The libraries

lib/system/system.dart
@@ -37,6 +37,21 @@ enum InterruptKind {
   breakPoint
 }
 
+class _Arguments
+    extends Object with UnmodifiableListMixin<String>, ListMixin<String>
+    implements List<String> {
+
+  _Arguments();
+
+  @native external int get length;
+
+  String operator[](int index) {
+    return _toString(index);
+  }
+
+  @native external static String _toString(int index);
+}
+
 /// This is a magic method recognized by the compiler, and references to it
 /// will be substituted for the actual main method.
 /// [arguments] is supposed to be a List<String> with command line arguments.
@@ -51,8 +66,8 @@ callMain(arguments) => invokeMain(arguments);
 
 /// This is the main entry point for a Dartino program, and it takes care of
 /// calling "main" and exiting the VM when "main" is done.
-void entry(int mainArity) {
-  Fiber.exit(callMain([]));
+void entry() {
+  Fiber.exit(callMain(new _Arguments()));
 }
 
 runToEnd(entry) {

pkg/dartino_compiler/lib/src/dartino_backend.dart
@@ -1375,7 +1375,6 @@ class DartinoBackend extends Backend
     // Reset the current system builder.
     newSystemBuilder(system);
 
-    commands.add(const PushNewInteger(0));
     commands.add(new PushFromMap(
         MapId.methods,
         system.lookupFunctionByElement(dartinoSystemEntry).functionId));

pkg/dartino_compiler/lib/vm_session.dart
@@ -259,7 +259,9 @@ class Session extends DartinoVmSession {
   }
 
   Future spawnProcess() async {
-    await runCommand(const ProcessSpawnForMain());
+    // TODO(ajohnsen): Handle arguments.
+    await runCommands([const PushNewInteger(0),
+                       const ProcessSpawnForMain()]);
   }
 
   Future run() async {

samples/simple_todo/simple_todo_main.cc
@@ -37,7 +37,7 @@ static void WaitForVmThread(int expected) {
 static void* StartDartino(void* arg) {
   char* snapshot_filepath_with_name = reinterpret_cast<char*>(arg);
   DartinoSetup();
-  DartinoRunSnapshotFromFile(snapshot_filepath_with_name);
+  DartinoRunSnapshotFromFile(snapshot_filepath_with_name, 0, NULL);
   DartinoTearDown();
   ChangeStatusAndNotify(kDone);
   return NULL;

samples/todomvc/todomvc_shared.cc
@@ -33,7 +33,7 @@ static void WaitForStatus(int expected) {
 static void* DartThreadEntry(void* arg) {
   const char* path = static_cast<char*>(arg);
   DartinoSetup();
-  DartinoRunSnapshotFromFile(path);
+  DartinoRunSnapshotFromFile(path, 0, NULL);
   DartinoTearDown();
   ChangeStatusAndNotify(kDone);
   return NULL;

src/shared/natives.h
@@ -126,6 +126,9 @@ namespace dartino {
                                                                              \
   N(ListIndexSet, "_FixedList", "[]=", false)                                \
                                                                              \
+  N(ArgumentsLength, "_Arguments", "length", false)                          \
+  N(ArgumentsToString, "_Arguments", "_toString", false)                     \
+                                                                             \
   N(ProcessSpawn, "Process", "_spawn", false)                                \
   N(ProcessQueueGetMessage, "Process", "_queueGetMessage", false)            \
   N(ProcessQueueSetupProcessDeath, "Process", "_queueSetupProcessDeath",     \
@@ -139,7 +142,7 @@ namespace dartino {
   N(StopwatchFrequency, "Stopwatch", "_frequency", false)                    \
   N(StopwatchNow, "Stopwatch", "_now", false)                                \
                                                                              \
-  N(TimerScheduleTimeout, "_DartinoTimer", "_scheduleTimeout", false)         \
+  N(TimerScheduleTimeout, "_DartinoTimer", "_scheduleTimeout", false)        \
   N(EventHandlerSleep, "<none>", "_sleep", false)                            \
                                                                              \
   N(ForeignLibraryLookup, "ForeignLibrary", "_lookupLibrary", false)         \

commit 8f38d060c8d34d971be5649d4f99cc56746daabf

Don't allocate arguments array when not spawning the main process.
This fixes a memory leak introduced in 37e79c13

TBR=kustermann@google.com
BUG=

Review URL: https://codereview.chromium.org/1700633002 .

src/vm/scheduler.cc
@@ -743,12 +743,14 @@ void SimpleProgramRunner::Run(int count,
 
     program->SetProgramExitListener(
         &SimpleProgramRunner::CaptureExitCode, this);
-    List<List<uint8>> arguments = List<List<uint8>>::New(argc);
-    for (int i = 0; i < argc; i++) {
-      uint8* utf8 = reinterpret_cast<uint8*>(strdup(argv[i]));
-      arguments[i] = List<uint8>(utf8, strlen(argv[i]));
+    if (process == NULL) {
+      List<List<uint8>> arguments = List<List<uint8>>::New(argc);
+      for (int i = 0; i < argc; i++) {
+        uint8* utf8 = reinterpret_cast<uint8*>(strdup(argv[i]));
+        arguments[i] = List<uint8>(utf8, strlen(argv[i]));
+      }
+      process = program->ProcessSpawnForMain(arguments);
     }
-    if (process == NULL) process = program->ProcessSpawnForMain(arguments);
     scheduler->ScheduleProgram(program, process);
   }
 