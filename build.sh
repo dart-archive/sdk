40ef9cebfc6edee159aa1da4066a83d4147acaf5
Martin Kustermann <kustermann@google.com>
2016-03-02 15:46:03 +0100

    Add ARM support to build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,23 @@
 DARTINO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LLVM_BIN=$DARTINO_ROOT/third_party/llvm/llvm-build-release/bin
 
+ARCH=IA32
+COMPILER=g++
+COMPILER_ARG=-m32
+LLVM_ARCH=x86
+LLC_ARG=
+
 if [ ! -f "$1" ]; then
   echo "Usage: $0 file.snapshot";
   exit 1;
+fi
+
+if [ "$2" = "arm" ]; then
+  ARCH="XARM"
+  COMPILER="arm-linux-gnueabihf-g++"
+  COMPILER_ARG=""
+  LLVM_ARCH="arm"
+  echo "Using XARM for building"
 fi
 
 function run {
@@ -34,11 +48,12 @@ run ninja -C out/DebugX64
 
 # Used for linking the generated llvm code with dartino runtime and a very small
 # embedder.
-run ninja -C out/ReleaseIA32 llvm_embedder
-run ninja -C out/DebugIA32 llvm_embedder
+run ninja -C out/Release$ARCH llvm_embedder
+run ninja -C out/Debug$ARCH llvm_embedder
 
 run rm -f $EXECUTABLE
 run rm -f $BASENAME.bc $BASENAME.ll $BASENAME.S $BASENAME.o
+run rm -f ${BASENAME}_opt.bc ${BASENAME}_opt.ll ${BASENAME}_opt.S ${BASENAME}_opt.o
 
 run out/DebugX64/llvm-codegen $SNAPSHOT $BASENAME.bc
 
@@ -50,10 +65,15 @@ run $LLVM_BIN/llvm-dis $BASENAME.bc -o $BASENAME.ll
 run $LLVM_BIN/llvm-dis ${BASENAME}_opt.bc -o ${BASENAME}_opt.ll
 
 # Compile LLVM IR to 32-bit x86 asm code.
-run $LLVM_BIN/llc -march=x86 -o $BASENAME.S $BASENAME.bc
-run $LLVM_BIN/llc -march=x86 -o ${BASENAME}_opt.S ${BASENAME}_opt.bc
+run $LLVM_BIN/llc -march=$LLVM_ARCH -o $BASENAME.S $BASENAME.bc
+run $LLVM_BIN/llc -march=$LLVM_ARCH -o ${BASENAME}_opt.S ${BASENAME}_opt.bc
+
+# Don't know why, but llc's emitted .S file contains some floating point ABI
+# annotation which the linker doesn't like.
+sed -i 's/^\(.*Tag_ABI_FP_number_model\)$/@\1/g' ${BASENAME}_opt.S
+sed -i 's/^\(.*Tag_ABI_FP_number_model\)$/@\1/g' ${BASENAME}.S
 
 # Link generated code together with dartino runtime and llvm embedder.
-run g++ -m32 -o $BASENAME -Lout/ReleaseIA32 -Lout/ReleaseIA32/obj/src/vm -lllvm_embedder -ldartino -ldl -lpthread $BASENAME.S
-run g++ -m32 -o ${BASENAME}_opt -Lout/ReleaseIA32 -Lout/ReleaseIA32/obj/src/vm -lllvm_embedder -ldartino -ldl -lpthread ${BASENAME}_opt.S
+run $COMPILER $COMPILER_ARG -o $BASENAME -Lout/Release$ARCH -Lout/Release$ARCH/obj/src/vm -lllvm_embedder -ldartino -ldl -lpthread $BASENAME.S
+run $COMPILER $COMPILER_ARG -o ${BASENAME}_opt -Lout/Release$ARCH -Lout/Release$ARCH/obj/src/vm -lllvm_embedder -ldartino -ldl -lpthread ${BASENAME}_opt.S
 
