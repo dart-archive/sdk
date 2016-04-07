1855fc668f6f6dd33cddcaf42f888edc3f43dcd0
Dan Rubel <danrubel@google.com>
2016-04-07 07:30:30 -0400

    disable analytics on bots
    R=whesse@google.com
    
    Review URL: https://codereview.chromium.org/1865833003 .

--- tools/bots/dartino.py
+++ tools/bots/dartino.py
@@ -240,6 +240,7 @@ def StepsTestSDK(debug_log, configuration):
   if os.path.exists(sdk_dir):
     shutil.rmtree(sdk_dir)
   Unzip(sdk_zip)
+  StepsDisableAnalytics(sdk_dir)
   build_conf = configuration['build_conf']
 
   def run():
@@ -251,7 +252,9 @@ def StepsTestSDK(debug_log, configuration):
 
 def StepsSanityChecking(build_dir):
   version = utils.GetSemanticSDKVersion()
-  dartino = os.path.join(build_dir, 'dartino-sdk', 'bin', 'dartino')
+  sdk_dir = os.path.join(build_dir, 'dartino-sdk')
+  StepsDisableAnalytics(sdk_dir)
+  dartino = os.path.join(sdk_dir, 'bin', 'dartino')
   # TODO(ricow): we should test this as a normal test, see issue 232.
   dartino_version = subprocess.check_output([dartino, '--version']).strip()
   subprocess.check_call([dartino, 'quit'])
@@ -264,6 +267,21 @@ def StepsSanityChecking(build_dir):
   if dartino_vm_version != version:
     raise Exception('Version mismatch, VERSION file has %s, dartino vm has %s' %
                     (version, dartino_vm_version))
+
+def StepsDisableAnalytics(sdk_dir):
+  dartino = os.path.join(sdk_dir, 'bin', 'dartino')
+  print "Disabling analytics"
+  try:
+    # TODO(danrubel) remove this once old approach has been removed
+    result = subprocess.check_output([dartino, '--no-analytics'])
+  except subprocess.CalledProcessError as exception:
+    result = "Ignoring dartino --no-analytics error: %s" % (error)
+  print result
+  try:
+    result = subprocess.check_output([dartino, 'disable', 'analytics'])
+  except subprocess.CalledProcessError as exception:
+    result = "Ignoring dartino disable analytics error: %s" % (error)
+  print result
 
 def StepsCreateDebianPackage():
   with bot.BuildStep('Create arm agent deb'):
