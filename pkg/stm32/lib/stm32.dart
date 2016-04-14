bbbfd886304e2b59bd0249754ac39c1541573508
Søren Gjesse <sgjesse@google.com>
2016-04-14 13:35:06 +0200

    Add main library to the stm32 package
    This is required to generate the documentation when the SDK is build.
    
    R=karlklose@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1885363002 .

---
+++ pkg/stm32/lib/stm32.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, the Dartino project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE.md file.
+
+/// Support for STM32 boards.
+/// Currently there is explicit support for the following boards:
+///
+/// STM32F746G Discovery
+library stm32;
