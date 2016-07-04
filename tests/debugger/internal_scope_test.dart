6f32a6a2f0fdb266622f55bddde06519fc3c35e0
Sigurd Meldgaard <sigurdm@google.com>
2016-07-04 15:15:37 +0200

    Use the "actual" frame number when printing all variables.
    This closes issue #565.
    
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org/2118233002 .

---
+++ tests/debugger/internal_scope_test.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2016, the Dartino project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE.md file.
+
+// DartinoDebuggerCommands=b main,r,n
+// Enable view of internal frames.
+// DartinoDebuggerCommands=toggle internal
+// Step into an internal function (Random.nextInt)
+// DartinoDebuggerCommands=s,s,s
+// DartinoDebuggerCommands=bt,p
+// Disable view of internal frames.
+// DartinoDebuggerCommands=toggle internal
+// Now we should get the stack view of the top current non-internal frame(main).
+// DartinoDebuggerCommands=bt,p
+// DartinoDebuggerCommands=q
+
+import 'dart:math';
+
+main() {
+  var rnd = new Random();
+  rnd.nextInt(255);
+}
