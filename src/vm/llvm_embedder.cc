26f7035301be984ecb08efc7a85185752a6b6e15
Dmitry Olshansky <dmitryolsh@google.com>
2016-10-21 12:45:40 +0200

    Remove missing include
    BUG=
    R=erikcorry@google.com
    
    Review URL: https://codereview.chromium.org/2438763003 .

--- src/vm/llvm_embedder.cc
+++ src/vm/llvm_embedder.cc
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE.md file.
 
-#include "src/vm/llvm_embedder.h"
-
 #include <stdlib.h>
 
 #include "include/dartino_api.h"
